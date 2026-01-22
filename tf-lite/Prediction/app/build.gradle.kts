a30d6dfd78ef485d6e896d691ac98649f03d1691
Chairoel <projectchairul@gmail.com>
2026-01-23 00:00:06 +0700

    feat: prediction Exercise using Regression with Interpreter

--- tf-lite/Prediction/app/build.gradle.kts
+++ tf-lite/Prediction/app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.play.services.tflite.java)
+    implementation(libs.play.services.tflite.gpu)
 }
--- tf-lite/Prediction/app/src/main/java/com/mascill/latihantensorflowliteprediction/MainActivity.kt
+++ tf-lite/Prediction/app/src/main/java/com/mascill/latihantensorflowliteprediction/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.mascill.latihantensorflowliteprediction
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.mascill.latihantensorflowliteprediction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var predictionHelper: PredictionHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        predictionHelper = PredictionHelper(
+            context = this,
+            onResult = { result ->
+                binding.tvResult.text = result
+            },
+            onError = {errorMessage ->
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        binding.btnPredict.setOnClickListener {
+            val input = binding.etSales.text.toString()
+            predictionHelper.predict(input)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        predictionHelper.close()
     }
 }
